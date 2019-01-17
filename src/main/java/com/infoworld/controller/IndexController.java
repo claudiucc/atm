7226305b6fdb636f8f3911a112edd5ec35fffefd
claudiucc <peetrr04@gmail.com>
2019-01-17 11:37:04 +0200

    init

---
+++ src/main/java/com/infoworld/controller/IndexController.java
@@ -0,0 +1,23 @@
+package com.infoworld.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by Claudiu on 01/17/2018.
+ */
+@Controller
+public class IndexController {
+
+    private final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView index() {
+        ModelAndView model = new ModelAndView("index");
+        return model;
+    }
+}
